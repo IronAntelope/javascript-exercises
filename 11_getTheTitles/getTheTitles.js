d7f2f6448dcb45fda8682ef6d99a01213660d3ef
uriah <108307211+IronAntelope@users.noreply.github.com>
2024-05-23 17:27:54 +0930

    Complete getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,19 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+  return arr.map(book => book.title);
 };
+
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
+console.log(typeof(books));
+console.log(books[0]);
 
 // Do not edit below this line
 module.exports = getTheTitles;
