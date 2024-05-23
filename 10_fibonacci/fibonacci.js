d8565a3cb6810da7b1477f542c213f354c074b21
uriah <108307211+IronAntelope@users.noreply.github.com>
2024-05-23 16:14:57 +0930

    Complete fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function fib(n) {
+  return n < 0 ? "OOPS"
+  : n == 0 ? 0
+  : n <= 2 ? 1
+  : fib(n -1) + fib(n - 2);
 };
 
 // Do not edit below this line
