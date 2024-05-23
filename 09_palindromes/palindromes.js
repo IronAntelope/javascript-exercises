b437f46a8cd0594d70cc8a7556ea2e3a03cde41b
uriah <108307211+IronAntelope@users.noreply.github.com>
2024-05-23 15:06:48 +0930

    Complet palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let arr = str.toLowerCase().replace(/[!.,' ']/g, "").split("");
+  let revArr = [...arr].reverse();
+  console.log(arr);
+  console.log(revArr);
 
+  let newStr = arr.toString();
+  let newRevStr = revArr.toString();
+
+  return (newStr === newRevStr) ? true : false;
 };
+
+// console.log(palindromes('ZZZZ car, a man, a maracaz.'));
 
 // Do not edit below this line
 module.exports = palindromes;
